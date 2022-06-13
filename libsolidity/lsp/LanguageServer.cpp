a31b0327a962b220dd2911fc7e6e568bdd48d302
Christian Parpart <christian@parpart.family>
2022-06-13 15:56:55 +0200

    lsp: Always load all solidity files from project for analyzing.

--- libsolidity/lsp/LanguageServer.cpp
+++ libsolidity/lsp/LanguageServer.cpp
@@ -31,6 +31,7 @@
 #include <liblangutil/SourceReferenceExtractor.h>
 #include <liblangutil/CharStream.h>
 
+#include <libsolutil/CommonIO.h>
 #include <libsolutil/Visitor.h>
 #include <libsolutil/JSON.h>
 
@@ -148,6 +149,11 @@ void LanguageServer::changeConfiguration(Json::Value const& _settings)
 	m_settingsObject = _settings;
 }
 
+vector<boost::filesystem::path> LanguageServer::allSolidityFilesFromProject() const
+{
+	return util::findFilesRecursively(m_fileRepository.basePath(), [](auto const& p) { return p.extension() == "sol"; });
+}
+
 void LanguageServer::compile()
 {
 	// For files that are not open, we have to take changes on disk into account,
@@ -156,6 +162,14 @@ void LanguageServer::compile()
 	FileRepository oldRepository(m_fileRepository.basePath());
 	swap(oldRepository, m_fileRepository);
 
+	// Load all solidity files from project.
+	for (auto const& projectFile: allSolidityFilesFromProject())
+		m_fileRepository.setSourceByUri(
+			projectFile.generic_string(),
+			util::readFileAsString(projectFile)
+		);
+
+	// Overwrite all files as opened by the client and might potentially have changes.
 	for (string const& fileName: m_openFiles)
 		m_fileRepository.setSourceByUri(
 			fileName,
